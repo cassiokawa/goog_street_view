4bf067c97e2d7fc6b7f026032a19161f32d20753
Cassio Kawakita <cassio.kawakita@gmail.com>
2018-09-27 07:42:13 -0300

    Updated with Brooklin main streets

--- streetview.py
+++ streetview.py
@@ -2,25 +2,58 @@ import urllib.request, urllib.parse, os
 import os
 google_street_view_api_key = os.environ.get('GOOGLE_STREET_VIEW_API_KEY')
 
-myloc = r"/images" #replace with your own location
+myloc = r"images" #replace with your own location
 key = google_street_view_api_key + "" #got banned after ~100 requests with no key
 
 def GetStreet(Add,SaveLoc):
-  base = "https://maps.googleapis.com/maps/api/streetview?size=1200x800&heading=90&location="
+  base = "https://maps.googleapis.com/maps/api/streetview?size=1200x800&heading=300&location="
   MyUrl = base + urllib.parse.quote_plus(Add) + key
   fi = Add + ".jpg"
   urllib.request.urlretrieve(MyUrl, os.path.join(SaveLoc,fi))
 
 
 Tests = [
-        "Rua Guararapes, Sao Paulo, SP",
-        "Rua Nebraska, Sao Paulo, SP"
+        "Rua Guararapes",
+        "Rua Nebraska",
+        "Rua Quintana",
+        "Rua Florida"
+        "Rua Texas",
+        "Rua Kansas",
+        "Rua Georgia",
+        "Rua Luisiania",
+        "Rua Pensilvania",
+        "Rua Indiana",
+        "Rua Michigan",
+        "Rua Arizona",
+        "Rua Castilho",
+        "Av Padre Jose Antonio dos Santos",
+
+
+        # "Av Santo Amaro",
+        # "Av Portugal",
+        # "Rua Nova York",
+        # "Rua California",
+        # "Rua Ribeiro do Vale",
+        # "Rua Guaraiuva",
+        # "Rua Conceicao de Monte Alegre",
+        # "Av Dr Chucri Zaidan",
+        # "Av Engenheiro Luis Carlos Berrini",
+        # "Av Nova Independencia",
+        # "Rua Arandu, Sao Paulo",
+        # "Rua Catipara",
+        # "Rua Ipurinas",
+        # "Rua Hollywood",
+        # "Rua Los Angeles",
+        # "Rua Tacoma",
+        # "Rua George Ohm",
+        # "Av Jornalista Roberto Marinho",
+        
         ]
 enderecos = []
 for i in Tests:
     counter = 10
-    while counter  < 2200:
-        endereco = str(counter) + " " + i
+    while counter  < 2500:
+        endereco = i + " " + str(counter)+ " , Sao Paulo, SP"
         print(endereco)
         enderecos.append(endereco)
         counter = counter + 100
